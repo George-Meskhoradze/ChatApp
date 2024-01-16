c1b751d350e53950c971c5785c93d03fd9a35410
Giorgi <meskhoradzegio@gmail.com>
2024-01-16 16:23:36 +0400

    Add Log In page which is not finished

--- FrontEnd/ChatAppDesign/src/Login.jsx
+++ FrontEnd/ChatAppDesign/src/Login.jsx
@@ -16,17 +16,45 @@ function Login() {
   // }, []);
 
   return (
-    <div className="bg bg-">
-      <div className="chat">
-        <form action="submit">
-          <label htmlFor="">Email</label>
-          <input type="text" placeholder="Enter Your Email"/>
+    <div className="bg w-full h-screen bg-chatapp flex justify-center items-center">
+      <div className="chat w-[350px] h-[400px] bg-lightPurple rounded-[6px] p-[15px]">
+        <form
+          action="submit"
+          className="form flex flex-col justify-center items-center gap-[30px]"
+        >
+          <h1 className="text-[35px] font-bold text-title">Log In</h1>
+          <div className="flex flex-col gap-[25px]">
+            <div className="flex flex-col justify-center gap-[5px]">
+              <label htmlFor="" className="text-title text-[16px] font-Raleway">
+                Email
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Your Email"
+                className="w-[300px] h-[40px] rounded-[5px] pl-[3px] outline-none"
+              />
+            </div>
 
-          <label htmlFor="">Password</label>
-          <input type="password" placeholder="Enter Your Password" />
-
-          <button type="submit"></button>
-          <p>Forgot Password?</p>
+            <div className="flex flex-col justify-center gap-[5px]">
+              <label htmlFor="" className="text-title text-[16px] font-Raleway">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                className="w-[300px] h-[40px] rounded-[5px] pl-[3px] outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-[10px]">
+            <button
+              type="submit"
+              className=" w-[300px] h-[40px] rounded-[5px] bg-orange text-title text-[18px] font-semibold"
+            >
+              Log In
+            </button>
+            <p className="text-title">Forgot Password?</p>
+          </div>
         </form>
       </div>
     </div>
---
+++ FrontEnd/ChatAppDesign/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'chatapp': "#151d2f",
+      "title": "#FFF",
+      "lightPurple": "#2d3250",
+      "orange": "#f9b17a"
+    },
+    fontFamily: {
+      "Raleway": "Raleway, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
